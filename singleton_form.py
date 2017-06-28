328e67c1de64cb70a311a1f6a02b16a06f3b2e7d
Michael Rinehart <mjr426@drexel.edu>
2017-06-28 10:32:29 -0400

    Refined requirements for singleton analysis

--- singleton_form.py
+++ singleton_form.py
@@ -21,31 +21,56 @@ def const_expon(expr):
     return isinstance(expr, Pow) and \
         isinstance(expr.args[1], (Number, NumberSymbol))
 
+
+'''
+    singleton_combinable_terms(expr): determines if two terms in a singleton
+        can be combined.
+            TODO LIST:
+                1. Two rational numbers are combinable (DONE!)
+                2. Two identical irrational numbers are combinable (Done!)
+                3. Two irrational numbers with combinable terms (pi, sqrt(2), etc)
+                    are combinable
+    returns:    true if so
+                false if not
+'''
+def singleton_combinable_terms(expr):
+    
+    #We don't want symbols or subexpressions like Add
+    for i in expr.args:
+        if not isinstance(i, (Number, NumberSymbol, Mul)):
+            return True
+
+        #If there's a product, make sure only one is rational
+        if isinstance(i, Mul):
+            if sum(isinstance(j, Number) for j in i.args) > 1:
+                return True
+
+    #Any two rational numbers can be simplified
+    if sum(isinstance(i, Rational) for i in expr.args) > 1:
+        return True
+   
+    #Evaluate each individual term numerically - if duplicates, return true
+    return len(expr.args) != len(set(map(N, expr.args)))
+
 '''
     is_singleton(expr) - determines if the expression is a singleton
     A singleton is defined to be either a number or a symbol, optionally raised
     to a power.
     returns:    true if so
                 false if not
-    TODO: Potentially refine definition of singletons in the case of an irrational added to a rational
-    Note: internally, 0 + 6 will pass as a singleton. This is
+    Note: internally, 0 + 6 will pass as a singleton, because SymPy will convert 0 + 6 to 6. This is
         SymPy's fault; if the issue is handled it won't be
         here.
 '''
 def is_singleton(expr):
     
-    #If number or symbol
+    #If singular number or symbol
     if isinstance(expr, (Number, NumberSymbol, Symbol)):
         return True
     
     #Case of rational added to irrational
-    #TODO: Something more mathematically sound than counting arguments?
     if isinstance(expr, Add):
-        for i in expr.args:
-            #only numbers and numbersymbols allowed
-            if not isinstance(i, (Number, NumberSymbol)):
-                return False
-        return len(simplify(expr).args) == len(expr.args)
+        return not singleton_combinable_terms(expr)
 
     #If number or symbol raised to a power
     if isinstance(expr, Pow):
@@ -54,10 +79,7 @@ def is_singleton(expr):
         if const_to_const(expr):
             return False
         elif isinstance(expr.args[0], Add):
-            if not isinstance(i, (Number, NumberSymbol)):    
-                return False
-            else: #BANDAID BELOW: PLEASE FIX
-                return len(simplify(expr.args[0]).args) == expr.args[0].args
+            return not singleton_combinable_terms(expr)
         else:
             return True
     return False 
