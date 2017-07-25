ad6094b6561780e64fb0ebd03ef76a818f2ee3d4
Michael Rinehart <mjr426@drexel.edu>
2017-07-25 14:13:31 -0400

    Reducible determination of polynomials in real in complex fields

--- polynomial_form.py
+++ polynomial_form.py
@@ -2,7 +2,7 @@ from __future__ import division
 from sympy.functions.elementary.trigonometric import TrigonometricFunction
 from sympy.functions.elementary.trigonometric import InverseTrigonometricFunction
 import sympy
-from sympy import Add,Mul,rcollect,Number,NumberSymbol,sin,cos,Pow,Integer,Symbol,fraction,gcd,div,degree,Derivative
+from sympy import Add,Mul,rcollect,Number,NumberSymbol,sin,cos,Pow,Integer,Symbol,fraction,gcd,div,degree, Derivative, discriminant
 from .monomial_form import *
 from .form_utils import *
 from .form_output import *
@@ -37,69 +37,75 @@ def is_fully_expanded_polynomial(expr, eval_trig=False):
 
     return False, ErrorOutput.strout("ERROR")
 
-def is_fully_factored_polynomial(expr, eval_trig=False, domain='Z'):
+def is_fully_factored_polynomial(expr, eval_trig=False, domain='RR'):
     '''Determines if a proper polynomial is fully expanded.
         A polynomial that is fully factored is defined as a sum or product of
             polynomials that cannot be reduced further.
         Args:
             expr: A standard sympy expression
+            domain: (optional) determination of the field that the polynomial \
+                is to be either reducible or irreducible over. Domain \
+                specification is determined by two capital leterts to match \
+                sympy's style.
+                Options:
+                    'RR' - Real numbers
+                    'CC' - Complex numbers
+                    TODO:
+                        'QQ' - Rationals
+                        'ZZ' - Integers
         Returns:
             a tuple containing:
                 [0] - boolean result of the function
                 [1] - string describing the result
     '''
 
-    #If the expression is a monomial or a singleton in the desired form
+    #If the expression is already a  monomial or a singleton in the desired form
     if is_monomial_form(expr)[0]:
         return True, PolynomialOutput.strout("IS_MONOMIAL")
 
     #Polynomials that are not squarefree by definition have
     #[d/d(symbol)](expr) as a factor, so it may be a good idea to factor
-    #those out
-
+    #those out for speed
     result = is_squarefree_polynomial(expr)
     if not result[0]:
         return result
     
 
-    if not isinstance(expr,Add):
-        result = is_numerically_reducible_monomial("expr")
-        if result[0]:
-            return False, result[1]
-    else:
+    if isinstance(expr, Add):
         for i in expr.args:
             result = is_numerically_reducible_monomial(i)
             if result[0]:
                 return False, result[1]
-
-    #Make sure each term in the polynomial is a monomial
-    for i in expr.args:
         result = is_factor_factored(i)
         if not result[0]:
             return result
-
-    #Currently, no definition of polynomials allows for monomials that
-    #have cancellable terms, so we can filter those out
-    result = is_numerically_reducible_monomial(expr)
-    if result[0]:
-        return False, result[1]
-
+    else:
+        result = is_numerically_reducible_monomial(expr)
+        if result[0]:
+            return False, result[1]
+        for i in expr.args:
+            result = is_factor_factored(i)
+            if not result[0]:
+                return False, result[1]
 
     #Currently, no definition of polynomials allows for monomials that
     #are combinable by integers, so we can filter those out
     result = const_divisible(expr)
     if result[0]:
         return False, result[1]
-    
-    if eval_trig:
-        result = sin_2_cos_2_simplifiable(expr)
-        if result[0]:
-            return False, result[1]
-    
+
     result = duplicate_bases(expr)
     if result[0]:
         return False, result[1]
-    return True, PolynomialOutput.strout("FACTORED")
+
+    if domain == 'RR':
+        result = real_field_reducible(expr)
+        return not result[0], result[1]
+    elif domain == 'CC':
+        result = complex_field_reducible(expr)
+        return not result[0], result[1]
+    else:
+        return False, UtilOutput.strout("ERROR")
 
 def is_squarefree_polynomial(expr):
     '''Determines if a polynomial is squarefree. If a polynomial is not
@@ -150,16 +156,57 @@ def is_factor_factored(expr):
             return False, result[1]
         if not all(is_factor_factored(j)[0] for j in expr.args):
             return False, PolynomialOutput.strout("NOT_FACTORED")
-    
-    #If the degree is larger than 2 and there are more than 1 terms, it's not factored
-    if isinstance(expr,Add):
-        if degree(expr) > 2 and len(expr.args) > 1:
-            return False, PolynomialOutput.strout("NOT_FACTORED")
-
-        elif degree(expr) > 1 and len(real_roots(expr)) == degree(expr):
-            return False, PolynomialOutput.strout("NOT_FACTORED")
 
     if duplicate_bases(expr)[0]:
         return False, PolynomialOutput.strout("NOT_FACTORED")
 
     return True, PolynomialOutput.strout("FACTORED")
+
+def complex_field_reducible(expr):
+    '''Determines if the polynomial is reducible over the complex field.
+    According to the fundamental theorem of algebra, a polynomial is reducible
+    if and only if the degree is one. However, for this library, we won't count
+    monomials such as x^4, as being reducible.
+    Args:
+        expr: a standard Sympy expression
+    Returns:
+        a tuple containing:
+            [0] - boolean result of the function
+            [1] - string describing the result
+    '''
+    result = is_monomial_form(expr)
+    if result[0]:
+        return False, "Expression is also a monomial"
+    
+    if degree(expr) > 1:
+        return True, "Expression has a degree higher than 1"
+    
+    return False, "Expression is simplified over the reals"
+
+def real_field_reducible(expr):
+    '''Determines if the polynomial is reducible over the real field.
+    According to the fundamental theorem of algebra, a polynomial is reducible
+    if and only if the following criterion are met:
+        1: Degree of polynomial is less than 3.
+        2: If degree of polynomial is 2, at least one of the roots are in
+            the complex field.
+    However, for this library, we won't count monomials, such as x^4, 
+        as being reducible.
+    Args:
+        expr: a standard Sympy expression
+    Returns:
+        a tuple containing:
+            [0] - boolean result of the function
+            [1] - string describing the result
+    '''
+    result = is_monomial_form(expr)
+    if result[0]:
+        return False, "Expression is also a monomial"
+
+    if degree(expr) > 2:
+        return True, "Expression has a degree higher than 1"
+
+    if degree(expr) == 2 and discriminant(expr) < 0:
+        return True, "Quadratic can be reduced further"
+
+    return False, "Expression can not be reduced further"
